d39c975e6bc99d99b278f65fde533f87851c15e9
Yuta Hayashibe <yuta@hayashibe.jp>
2024-01-30 20:06:41 +0900

    Add

---
+++ scripts/copy_models.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import argparse
+import shutil
+from pathlib import Path
+
+
+def operation(
+    *,
+    path_in: Path,
+    path_out: Path,
+) -> None:
+    for f in path_in.glob("**/mymodel.safetensors"):
+        myout = path_out.joinpath(f.parent.name + ".safetensors")
+        shutil.copy(f, myout)
+
+
+def get_opts() -> argparse.Namespace:
+    oparser = argparse.ArgumentParser()
+    oparser.add_argument("--input", "-i", type=Path, required=True)
+    oparser.add_argument("--output", "-o", type=Path, required=True)
+    return oparser.parse_args()
+
+
+def main() -> None:
+    opts = get_opts()
+    operation(
+        path_in=opts.input,
+        path_out=opts.output,
+    )
+
+
+if __name__ == "__main__":
+    main()
